3550d443072c0d6d061884420a3643b4499cc3c3
Tianjiao Huang <maitian519@gmail.com>
2018-01-20 22:22:58 -0800

    get search list done

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/map', (req, res, next) => {
+  res.render('map', {js:req.app.locals.js.concat('/js/map.js')});
+});
+
 module.exports = router;
