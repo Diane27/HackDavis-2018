290a7523a0b78fee868b6f6b19978712b15f1d9c
Tianjiao Huang <maitian519@gmail.com>
2018-01-20 21:20:02 -0800

    add some more stuff

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const app = express();
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    server = require('http').Server(app),
+    port = 8080,
+    bodyParser = require('body-parser');
+require('dotenv-safe').load();
+port = port || process.env.PORT;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var routes = require('./api/routes/routes.js');
+routes(app);
+app.use(express.static(__dirname + '/public'));
+server.listen(port);
 
-app.get('/', (req, res) => {
-  res.send('Hello HackDavis!');
-});
-
-const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log(`=> running at http://${host}:${port}`);
-});
+console.log("working on " + port);
