4a481558eb67860665f8056eb2291b0816be1749
Jesse Smick <janka102@gmail.com>
2018-01-20 23:56:16 -0800

    Add account route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const accounts = require('./accounts.json');
 
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -14,10 +15,21 @@ router.get('/map', (req, res, next) => {
 
 router.post('/login', (req, res, next) => {
   if (req.body.username === 'jack' && req.body.password === 'jill') {
-    res.json({ loggedIn: true });
+    res.json({ data: { loggedIn: true } });
   } else {
     res.status(401);
-    res.json({ loggedIn: false });
+    res.json({ error: 'Invalid credentials' });
+  }
+});
+
+router.get('/account', (req, res, next) => {
+  const username = req.body.username || req.query.username;
+
+  if (!username || !accounts[username]) {
+    res.status(400);
+    res.json({ error: 'Need username' });
+  } else {
+    res.json({ data: accounts[username] });
   }
 });
 
