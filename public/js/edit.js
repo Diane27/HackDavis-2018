e3355afff8d31678d43642312948126e9f477923
Tianjiao Huang <maitian519@gmail.com>
2018-01-21 10:10:30 -0800

    update storage after rmCls

--- public/js/edit.js
+++ public/js/edit.js
@@ -7,7 +7,8 @@ function removeClass(targetId, day) {
     // Set day to false
     // if all days are false, delete entry
     var id;
-    var classes = JSON.parse(window.localStorage.getItem('userData')).schedule;
+    var data = JSON.parse(window.localStorage.getItem('userData'));
+    var classes = data.schedule;
     
     var i = 0;
     for (; i < classes.length; ++i) {
@@ -21,10 +22,12 @@ function removeClass(targetId, day) {
     if (c.days.every(function (e) { !e } )) {
         classes.splice(i, 1);
     }
+
+    window.localStorage.setItem('userData', data);
 }
 
 function modifyClass(data) {
-    
+
 }
 
 function addClass(data) {
