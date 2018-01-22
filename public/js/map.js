2da13f8bcdc90c8c2caa1df66b6df3f033fbfd25
Jesse Smick <janka102@gmail.com>
2018-01-22 14:14:09 -0800

    Set current location on load, and disabled bounds b/c it was doing weird things

--- public/js/map.js
+++ public/js/map.js
@@ -1,64 +1,69 @@
 function getLocation(cb) {
   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(cb);
+    navigator.geolocation.getCurrentPosition(function(position) {
+      showPosition(position);
+      cb(position);
+    });
   } else {
-      alert("Geolocation is not supported by this browser.");
+    alert('Geolocation is not supported by this browser.');
   }
 }
+
 function showPosition(position) {
-  console.log("Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude);
+  console.log('Latitude: ' + position.coords.latitude + '\nLongitude: ' + position.coords.longitude);
 }
 // This example requires the Places library. Include the libraries=places
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 
 var map;
 var directionsService;
 var directionsDisplay;
 
 function initMap() {
-  // Create the map.
-  var pyrmont = {lat: 38.5382, lng: -121.7617};
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: pyrmont,
-    zoom: 17,
-    minZoom: 15,
-    maxZoom: 17,
-    fullscreenControl: false,
-    styles: [
-            {featureType: 'poi.school', elementType: 'geometry.fill', stylers: [{color: '#78be20'}]},
-            {featureType: 'landscape.man_made', elementType: 'geometry.fill', stylers: [{color: '#ECD47F'}]}]
+  // Create the map after getting current location
+
+  getLocation(function(position) {
+    var pyrmont = { lat: position.coords.latitude, lng: position.coords.longitude };
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: pyrmont,
+      zoom: 17,
+      minZoom: 14,
+      maxZoom: 19,
+      fullscreenControl: false,
+      styles: [
+        { featureType: 'poi.school', elementType: 'geometry.fill', stylers: [{ color: '#78be20' }] },
+        { featureType: 'landscape.man_made', elementType: 'geometry.fill', stylers: [{ color: '#ECD47F' }] }
+      ]
+    });
+
+    // Create the places service.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Perform a nearby search.
+    service.nearbySearch({ location: pyrmont, radius: 500, type: ['campus building'] },
+    function(results, status, pagination) {
+      if (status !== 'OK') return;
+
+      createMarkers(results);
+      // moreButton.disabled = !pagination.hasNextPage;
+      // getNextPage = pagination.hasNextPage && function() {
+      // pagination.nextPage();
+      // };
+    });
+
+    // Create the directions service
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
   });
-
-  // Create the places service.
-  var service = new google.maps.places.PlacesService(map);
-
-  // Perform a nearby search.
-  service.nearbySearch(
-      {location: pyrmont, radius: 500, type: ['campus building']},
-      function(results, status, pagination) {
-        if (status !== 'OK') return;
-
-        createMarkers(results);
-        // moreButton.disabled = !pagination.hasNextPage;
-        // getNextPage = pagination.hasNextPage && function() {
-          // pagination.nextPage();
-        // };
-      });
-
-      // Create the directions service
-      directionsService = new google.maps.DirectionsService();
-      directionsDisplay = new google.maps.DirectionsRenderer();
-      directionsDisplay.setMap(map);
 }
 
 function createMarkers(places) {
-  var bounds = new google.maps.LatLngBounds();
+  // var bounds = new google.maps.LatLngBounds();
   var placesList = document.getElementById('places');
 
-  for (var i = 0, place; place = places[i], i < 10; i++) {
+  for (var i = 0, place; (place = places[i]), i < 10; i++) {
     var image = {
       url: place.icon,
       size: new google.maps.Size(71, 71),
@@ -76,16 +81,18 @@ function createMarkers(places) {
 
     var li = document.createElement('li');
     li.innerHTML = `<a href="#map">${place.name}</a>`;
-    $(li).click((function(place) {
-      return function(){ //works but not the best way --Jesse
-        routeToBuilding(place.geometry.location);
-        console.log(place, map.getCenter());
-      };
-    })(place));
+    $(li).click(
+      (function(place) {
+        return function() {
+          // works but not the best way --Jesse
+          routeToBuilding(place);
+        };
+      })(place.geometry.location)
+    );
 
     placesList.appendChild(li);
 
-    bounds.extend(place.geometry.location);
+    // bounds.extend(place.geometry.location);
   }
-  map.fitBounds(bounds);
+  // map.fitBounds(bounds);
 }
